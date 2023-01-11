c845c8c187d3eaf6dc522c174924a3fa6bfc1844
NLarrea <larrea.naia@gmail.com>
2023-01-11 02:33:52 +0100

    EJERCICIOS/00_conceptos_basicos/00_introduccion.py added

---
+++ EJERCICIOS/00_conceptos_basicos/00_introduccion.py
@@ -0,0 +1,21 @@
+# VARIABLES
+
+# 01 - MENSAJE SIMPLE:
+# Asigna un mensaje a una variable e imprime el mensaje
+
+message = "Hello World!"
+print(message)              # Hello World!
+
+
+
+# 02 - MENSAJES SIMPLES:
+"""
+Asigna un mensaje a una variable e imprime el mensaje. Después, escribe otro mensaje
+guardándolo en la misma variable e imprime de nuevo el mensaje.
+"""
+
+message = "Hello World!"
+print(message)              # Hello World!
+
+message = "Hello Python"    # ha cambiado el valor / mensaje
+print(message)              # Hello Python
