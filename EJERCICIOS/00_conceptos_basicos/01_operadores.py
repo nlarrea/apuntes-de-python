3365b9705e592f0e03a08086ce62ff784f39a710
NLarrea <larrea.naia@gmail.com>
2023-01-11 02:34:24 +0100

    EJERCICIOS/00_conceptos_basicos/01_operadores.py added

---
+++ EJERCICIOS/00_conceptos_basicos/01_operadores.py
@@ -0,0 +1,33 @@
+# OPERADORES
+
+# 01 - NÚMERO 8
+"""
+Escribe una suma, resta, multiplicación y división para que su resultado sea el número 8.
+Asegúrate de escribir las operaciones dentro de un print() para ver los resultados. Tus
+líneas de código deben verse así: print(5+3)
+"""
+
+# suma
+print(6 + 2)
+
+# resta
+print(11 - 3)
+
+# multiplicación
+print(4 * 2)
+
+# división
+print(16 / 2)
+
+
+
+# 02 - NÚMERO FAVORITO
+"""
+Usa una variable para representar tu número favorito. Después, usando esa variable, crea un
+mensaje que revele cuál es tu número favorito. Imprime ese mensaje.
+"""
+
+favorite_number = 11
+message = f"My favorite number is {favorite_number}"
+
+print(message)
