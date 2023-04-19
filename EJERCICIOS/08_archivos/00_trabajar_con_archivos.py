bb2e2533c2768c3d21cebcd91095a598cd2aaae7
nlarrea <larrea.naia@gmail.com>
2023-04-19 22:45:46 +0200

    files - EJERCICIOS: trabajar con archivos

--- EJERCICIOS/08_archivos/00_trabajar_con_archivos.py
+++ EJERCICIOS/08_archivos/00_trabajar_con_archivos.py
@@ -62,3 +62,59 @@ In C you can ask users for input
 In C you can define functions and classes
 In C you can export and import data
 """
+
+
+
+print("03 - INVITADO\n")
+"""
+Escribe un programa que pida al usuario que escriba su nombre. Escribe el
+nombre introducido en un archivo llamado 'guest.txt'.
+"""
+
+user_name = input("Enter your name: ")
+
+file_path = "files/guest.txt"
+with open(file_path, "w") as file:
+    file.write(f"- {user_name}\n")
+
+
+
+print("\n\n04 - LISTA DE INVITADOS\n")
+"""
+Utiliza un bucle while para pedir el nombre de varios usuarios. Cuando escriban
+su nombre, muestra por pantalla un mensaje personalizado y escribe una línea
+que recuerde su visita en 'guest_book.txt'.
+"""
+
+file_path = "files/guest_book.txt"
+
+while True:
+    user_name = input("Enter your name: ")
+    if user_name != "" and user_name != "q":
+        print(f"Hello {user_name}! Have a nice day!")
+
+        with open(file_path, "a") as file:
+            file.write(f"- {user_name}\n")
+    else:
+        break
+
+
+
+print("\n\n05 - ENCUESTA\n")
+"""
+Crea un bucle while que pregunte al usuario por qué le gusta la programación.
+Mientras se escriban respuestas, añádelas a un archivo que contenga todas las
+respuestas.
+"""
+
+file_path = "files/poll_answers.txt"
+
+while True:
+    reason = input("Enter a reason why you like programming: ")
+    if reason != "" and reason != "q":
+        print(f"- {reason}")
+
+        with open(file_path, "a") as file:
+            file.write(f"- {reason}")
+    else:
+        break
