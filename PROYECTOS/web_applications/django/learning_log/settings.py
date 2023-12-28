c60e87cd2d2eeaef8f7e1f2a86eca807968ae834
nlarrea <larrea.naia@gmail.com>
2023-12-28 15:42:54 +0100

    web_applications - django: using .env for settings' secret key

--- PROYECTOS/web_applications/django/learning_log/settings.py
+++ PROYECTOS/web_applications/django/learning_log/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tk2lk6df%ao)g*1*umv32r-if*g0@mxc)$l+(*pw!7pxcg)dm^'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
